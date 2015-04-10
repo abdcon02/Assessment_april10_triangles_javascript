969d2d2dad1ae74ab35565153347fa9bf2a772b9
Connor Abdelnoor <connor.abd@gmail.com>
2015-04-10 09:33:09 -0700

    Create tests and passing function for non-triangles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 function makeTriangle(a,b,c){
-    if(a === b && b === c && c === a){
-        return 'equilateral';
-    } else if( a === b || b === c || c === a ){
-        return 'isosceles';
-    } else {
-        return 'scalene';
+  var desc_number_array = [a,b,c].sort(function(x,y) {
+    return y-x;
+  });
+  console.log(desc_number_array);
+  console.log(desc_number_array[0] >= (desc_number_array[1] + desc_number_array[2]));
+  if (desc_number_array[0] >= desc_number_array[1] + desc_number_array[2]){
+    return "This is not a triangle";
+  } else if(a === b && b === c && c === a){
+    return 'equilateral';
+  } else if(a === b || b === c || c === a ){
+    return 'isosceles';
+  } else {
+    return 'scalene';
     };
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,8 +3,12 @@ describe('makeTriangle', function() {
         expect(makeTriangle(1,1,1)).to.equal('equilateral')
     });
 
-    it("takes a triangle with a=1, b=1, c=3 and returns isosceles", function() {
-        expect(makeTriangle(1,1,3)).to.equal('isosceles')
+    it("takes a triangle with a=2, b=2, c=2 and returns equilateral", function() {
+        expect(makeTriangle(2,2,2)).to.equal('equilateral')
+    });
+
+    it("takes a triangle with a=2, b=2, c=3 and returns isosceles", function() {
+        expect(makeTriangle(2,2,3)).to.equal('isosceles')
     });
 
     it("takes a triangle with a=3, b=1, c=3 and returns isosceles", function() {
@@ -15,11 +19,23 @@ describe('makeTriangle', function() {
         expect(makeTriangle(2,3,3)).to.equal('isosceles')
     });
 
-    it("takes a triangle with a=2, b=3, c=6 and returns scalene", function() {
-        expect(makeTriangle(2,3,6)).to.equal('scalene')
+    it("takes a triangle with a=2, b=3, c=4 and returns scalene", function() {
+        expect(makeTriangle(2,3,4)).to.equal('scalene')
     });
 
-    it("takes a triangle with a=1, b=3, c=7 and returns scalene", function() {
-        expect(makeTriangle(1,3,7)).to.equal('scalene')
+    it("takes a triangle with a=5, b=6, c=10 and returns scalene", function() {
+        expect(makeTriangle(5,6,10)).to.equal('scalene')
+    });
+
+    it("takes a triangle with a=4, b=5, c=7 and returns scalene", function() {
+        expect(makeTriangle(7,5,4)).to.equal('scalene')
+    });
+
+    it("takes a triangle with a=1, b=1, c=7 and returns This is not a triangle", function() {
+        expect(makeTriangle(1,1,7)).to.equal('This is not a triangle')
+    });
+
+    it("takes a triangle with a=2, b=8, c=2 and returns This is not a triangle", function() {
+        expect(makeTriangle(2,8,2)).to.equal('This is not a triangle')
     });
 })
