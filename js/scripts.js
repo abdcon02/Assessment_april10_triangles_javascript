dfbf4311d0940a09374cb65178f643bc81748b03
Connor Abdelnoor <connor.abd@gmail.com>
2015-04-10 09:10:14 -0700

    Create tests and passing function for isosceles triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function makeTriangle(a,b,c){
     if(a === b && b === c && c === a){
         return 'equilateral';
+    } else if( a === b || b === c || c === a ){
+        return 'isosceles'
     };
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,16 @@ describe('makeTriangle', function() {
     it("takes a triangle with a=1, b=1, c=1 and returns equilateral", function() {
         expect(makeTriangle(1,1,1)).to.equal('equilateral')
     });
+
+    it("takes a triangle with a=1, b=1, c=3 and returns isosceles", function() {
+        expect(makeTriangle(1,1,3)).to.equal('isosceles')
+    });
+
+    it("takes a triangle with a=3, b=1, c=3 and returns isosceles", function() {
+        expect(makeTriangle(3,1,3)).to.equal('isosceles')
+    });
+
+    it("takes a triangle with a=2, b=3, c=3 and returns isosceles", function() {
+        expect(makeTriangle(2,3,3)).to.equal('isosceles')
+    });
 })
